a87b8e53542a2ca205062352e37f16f44cb92dcc
pgore <PalashGore@users.noreply.github.com>
2019-10-18 09:57:41 -0400

    Fix

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ fi
 
 echo "Building docker image with tag :$VERSION"
 cd C:/projects/dp/dp-server
-docker build -t gcr.io/cloudpark-e6cc4/server-image:$VERSION .
-gcloud docker -- push gcr.io/cloudpark-e6cc4/server-image:$VERSION
+docker build -t gcr.io/cloudpark-e6cc4/dp-server:$VERSION .
+gcloud docker -- push gcr.io/cloudpark-e6cc4/dp-server:$VERSION
